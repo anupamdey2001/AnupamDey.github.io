6098764fb5dc0bbcbc229c359484e16a2fc664fb
Anupam Dey <79352396+anupamdey2001@users.noreply.github.com>
2021-08-08 17:44:41 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    });
+});
